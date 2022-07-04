3bcd885c525f1ccf188d55e69c2cf3b7dde36725
LoOW3 <ignaciodiaznanni@gmail.com>
2022-07-04 20:00:18 -0300

    create basic ecommerce

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { client } from '../lib/client'
+import { Product, FooterBanner, HeroBanner } from '../components';
 
-const index = () => {
+const Home = ({ products, bannerData }) => {
   return (
-    <div>index</div>
+    <>
+      <HeroBanner HeroBanner={bannerData.length && bannerData[0]}/>
+      <div className='products-heading'>
+        <h2>Best Selling Products</h2>
+        <p>Spreakers of many variations</p>
+      </div>
+      <div className='products-container'>
+        {products?.map(p => <Product key={p._id} p={p}/>)}
+      </div>
+
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+    </>
   )
 }
 
-export default index
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+}
+
+export default Home
