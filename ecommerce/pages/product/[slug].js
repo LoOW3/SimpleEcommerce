7013a403dfa9e9f487c96736c349f2d6c66890b2
LoOW3 <ignaciodiaznanni@gmail.com>
2022-07-07 01:02:33 -0300

    last changes

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -8,8 +8,13 @@ const ProductDetails = ({ product, products}) => {
 
     const { image,name, details, price} = product
     const [index,setIndex] = useState(0);
-    const { decQty, incQty, qty, onAdd } = useStateContext();
+    const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
+    const handleBuyNow = () => {
+        onAdd(product, qty);
+
+        setShowCart(true)
+    }
   return (
     <div>
         <div className='product-detail-container'>
@@ -47,13 +52,13 @@ const ProductDetails = ({ product, products}) => {
                     <h3>Quantity:</h3>
                     <p className='quantity-desc'>
                         <span className='minus' onClick={decQty}><AiOutlineMinus/></span>
-                        <span className='num' onClick=''>{qty}</span>
+                        <span className='num'>{qty}</span>
                         <span className='plus' onClick={incQty}><AiOutlinePlus/></span>
                     </p>
                 </div>
                 <div className='buttons'>
                     <button type='button' className='add-to-cart' onClick={() => onAdd(product, qty)}>Add to cart</button>
-                    <button type='button' className='buy-now' onClick=''>Buy now</button>
+                    <button type='button' className='buy-now' onClick={handleBuyNow}>Buy now</button>
                 </div>
             </div>
         </div>
